9d3107ee393cb156e51b8836609d170e74aadf0c
Tim Schraml <tim@bergfreunde.de>
2023-12-01 13:35:12 +0100

    add local runtime

---
+++ katas/whistlebird/api/api/server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Read the .env file.
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import("../src/app.js"));
+try {
+  await app.ready();
+  await app.listen({ port: 3000 })
+} catch (err) {
+  app.log.error(err)
+  process.exit(1)
+}
