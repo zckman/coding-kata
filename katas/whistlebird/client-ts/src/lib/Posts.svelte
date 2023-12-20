2c424384049bec966cb2b8f435deed0a24731f85
Dennis Syhre <dennis.syhre@bergfreunde.de>
2023-12-20 08:15:08 +0100

    Add Posts

---
+++ katas/whistlebird/client-ts/src/lib/Posts.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import Api from './Api';
+  import Post from './Post.svelte';
+
+  interface User {
+    name: string;
+    image: string;
+  }
+
+  interface PostData {
+    user: User;
+    message: string;
+    created_at: string;
+  }
+
+  let posts: PostData[] = [];
+
+  onMount(async () => {
+    posts = await Api.getPosts();
+  });
+</script>
+
+
+{#each posts as post (post.created_at)}
+    <div class="post mb-4 mt-4 shadow-sm">
+        <Post class="post" {post} />
+    </div>
+{/each}
+
+<style lang="scss">
+  .post {
+  }
+</style>
