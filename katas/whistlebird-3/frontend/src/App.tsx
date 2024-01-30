3c7ac850142a2724a5130d6908f3a3d217ef3ab0
aleschneck <alejandro.schneck@bergfreunde.de>
2024-01-30 11:41:40 +0100

    add form to frontend

--- katas/whistlebird-3/frontend/src/App.tsx
+++ katas/whistlebird-3/frontend/src/App.tsx
@@ -10,7 +10,11 @@ function App() {
   const { data, error, isLoading } = useSWR('api/posts', fetcher, { refreshInterval: 1000 })
 
   return (
-    <main className="max-w-3xl mx-auto">
+    <main className="max-w-3xl mx-auto p-2">
+      <form onSubmit={() => {}} className="flex py-2 font-mono">
+        <input type="text" name="message" placeholder="message" className='flex-1 border rounded-l p-2' />
+        <button type="submit" className="py-2 px-4 rounded-r bg-slate-200">send</button>
+      </form>
       {isLoading && (<p className="py-2 text-sky-500 text-base font-mono font-medium">loading...</p>)}
       {!error && !isLoading && data && data.map(({ message, created_at, user }, i) => (
         <div key={`${i}_${message}`} className="flex flex-col py-2 font-mono font-medium border-b border-slate-200">
