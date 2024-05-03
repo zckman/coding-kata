95e8f512d8d2664c0c65176331026df79cf96ee2
Dennis Syhre <dennis.syhre@bergfreunde.de>
2024-05-03 15:06:06 +0200

    Practice 2

--- katas/zustand/src/components/TodoList.tsx
+++ katas/zustand/src/components/TodoList.tsx
@@ -1,12 +1,23 @@
-import { FC, useState } from "react";
+import {FC, FormEvent, useState} from "react";
 import DarkModeToggle from "./DarkModeToggle";
+import {useTodoStore} from "../store/todoStore";
+import {useDarkModeStore} from "../store/darkModeStore";
 
 const TodoList: FC = () => {
   const [todoValue, setTodoValue] = useState("");
 
-  const handleSubmit = () => {};
+  const todos = useTodoStore(state => state.todos)
+  const addTodo = useTodoStore(state => state.addTodo)
+  const deleteTodo = useTodoStore(state => state.deleteTodo)
+  const completeTodo = useTodoStore(state => state.completeTodo)
+
+  const handleSubmit = (e: FormEvent) => {
+    addTodo(todoValue)
+    e.preventDefault()
+  };
   //TODO: This state should be updated dynamically with the toggle
-  const darkMode = false;
+  const darkMode = useDarkModeStore((state) => state.darkMode);
+
   return (
     <div
       className={`w-full max-w-md p-4 shadow-md rounded-lg min-w-32 ${
@@ -33,9 +44,11 @@ const TodoList: FC = () => {
               ? "border-gray-600 focus:ring-blue-300"
               : "border-gray-300 focus:ring-blue-500"
           } outline-none`}
+          autoComplete="off"
         />
         <button
           type="submit"
+          disabled={todoValue.trim().length === 0}
           className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
         >
           Add
@@ -43,7 +56,9 @@ const TodoList: FC = () => {
       </form>
 
       <ul className="mt-4">
+        { todos.map((todo) => (
           <li
+            key={todo.id}
             className={`flex justify-between items-center p-2 rounded mt-2 shadow ${
               darkMode ? "bg-gray-700" : "bg-gray-50"
             }`}
@@ -54,10 +69,14 @@ const TodoList: FC = () => {
                 darkMode ? "text-gray-200" : "text-gray-900"
               }`}
             >
+              { todo.text }
             </span>
             <div className="flex flex-row gap-4">
                 <button
                   // complete todo onClick
+                  onClick={() => {
+                    completeTodo(todo.id)
+                  }}
                   className="px-4 text-sm bg-green-500 hover:bg-green-600 text-white p-1 rounded transition duration-200"
                 >
                   Done
@@ -65,12 +84,16 @@ const TodoList: FC = () => {
 
               <button
                 // delete Todo onclick
+                onClick={() => {
+                  deleteTodo(todo.id)
+                }}
                 className="px-4 text-sm bg-red-500 hover:bg-red-600 text-white p-1 rounded transition duration-200"
               >
                 Delete
               </button>
             </div>
           </li>
+        )) }
       </ul>
       <DarkModeToggle />
     </div>
--- katas/zustand/src/store/todoStore.ts
+++ katas/zustand/src/store/todoStore.ts
@@ -1,5 +1,33 @@
-
 // create a Todo store
 // use the todoTypes as types
 
-export {}
+import {DarkModeState} from "../types/darkmodeTypes";
+import {Todo, TodoState} from "../types/todoTypes"
+import {create} from 'zustand'
+
+export const useTodoStore = create<TodoState>((set) => ({
+  todos: [],
+  addTodo: (text: string) => set((state) => {
+    const id = (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)).toString(16)
+    const todos = [...state.todos]
+    todos.push({
+      text,
+      id,
+      isCompleted: false,
+    })
+    return { todos }
+  }),
+  deleteTodo: (id: string) => set((state) => ({
+    todos: state.todos.filter((todo) => todo.id !== id)
+  })),
+  completeTodo: (id: string) => set((state) => {
+    const todos = state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isCompleted = true
+      }
+      return todo
+    })
+
+    return { todos }
+  }),
+}))
